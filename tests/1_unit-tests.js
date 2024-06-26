987adad3355c6dcc9c6716775f20b2aa3b0d45f0
nahiriv <nahir.riveros.work@gmail.com>
2024-06-25 23:41:27 -0400

    Update 1_unit-tests.js

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,17 +1,16 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-
 suite('Unit Tests', function () {
   suite('Basic Assertions', function () {
-    // 1 - Use assert.isNull() or assert.isNotNull() to make the tests pass.
+    /** 1 - Use assert.isNull() or assert.isNotNull() to make the tests pass. **/
     test('#isNull, #isNotNull', function() {
-      assert.isNull(null, 'this is an optional error description - e.g. null is null');
-      assert.isNotNull(1, '1 is not null');
-    });
-  });
+    assert.isNull(
+      null,
+    'this is an optional error description - e.g. null is null'
+  );
+  assert.isNotNull(1, '1 is not null');
 });
-
     // #2
     test('#isDefined, #isUndefined', function () {
       assert.fail(null, 'null is not undefined');
